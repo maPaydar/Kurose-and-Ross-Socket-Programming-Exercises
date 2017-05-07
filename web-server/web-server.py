00c43dd3c9cad1a84c946583c34c5115a52c0de8
Amin Paydar <mapsgp920@gmail.com>
2017-05-07 22:54:13 +0430

    change http handler package

--- web-server/web-server.py
+++ web-server/web-server.py
@@ -1,6 +1,6 @@
-from sheard.HttpHandler import HttpHandler
-from sheard.TcpServer import TcpServer
 from sheard.ClientThread import ClientThread
+from sheard.TcpServer import TcpServer
+from .HttpHandler import HttpHandler
 
 if __name__ == '__main__':
     server = TcpServer()
